ed3696f2aa7cfb1e1c9c849380ce23b9597e656e
reza <moradzade.mohamadreza@gmail.com>
2025-06-05 08:41:56 -0700

    third commit

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  ResultScreen({super.key, this.correctAnswer = 0});
+
+  int correctAnswer = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red[800],
+        title: Text(
+          'نتیجه آزمون',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Image(
+                image: AssetImage('images/cup.png'),
+              ),
+            ),
+            Text(
+              'پاسخ های صییح شما',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              '$correctAnswer',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 100,
+              ),
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
